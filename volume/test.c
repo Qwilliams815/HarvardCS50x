1bede6a3e1517803345dbd17346970b765e5c0b0
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-05 22:38:31 +0000

    Wed, Jan 5, 2022, 10:38 PM +00:00

--- volume/test.c
+++ volume/test.c
@@ -8,7 +8,10 @@ int main(void)
 {
     char *n = "Hello!";
     //int *p = n;
-    printf("%p\n", n);
-    printf("%c\n", n[4]);
+    //printf("%p\n", n);
+    //printf("%c\n", n[4]);
+    FILE *input = fopen("input.wav", "r");
+
+    printf("%s", input);
 
 }
