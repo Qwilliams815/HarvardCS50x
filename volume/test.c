14d9e24848376da20810472027cea48fc8c9cdd0
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-05 22:16:11 +0000

    Wed, Jan 5, 2022, 10:16 PM +00:00

---
+++ volume/test.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n = 50;
+    int *p = n;
+    printf("%p\n", p);
+
+}
