21e596345fc7fa98ed897d08be9133c65a89843e
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-06 02:25:47 +0000

    Thu, Jan 6, 2022, 2:25 AM +00:00

--- volume/test.c
+++ volume/test.c
@@ -5,29 +5,12 @@
 #include <stdlib.h>
 
 //typedef uint8_t BYTE;
-//typedef int16_t BYTE2;
+typedef int16_t BYTE2;
 
 int main(void)
 {
-    //char *n = "Hello!";
-    //int *p = n;
-    //printf("%p\n", n);
-    //printf("%c\n", n[4]);
+    int16_t test;
 
-    FILE *input = fopen("input.wav", "r");
-
-    uint8_t header[44];
-    fread(header, 1, 44, input);
-    fwrite(header, 1, 44, output);
-
-    fwrite()
-
-    for (int i=0; i<44; i++) {
-        printf("%i ", header[i]);
-    }
-
-    printf("\n");
-
-    fclose(input);
-
+    //test[0] = 5;
+    printf("%i", test);
 }
