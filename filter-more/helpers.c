adb52602b0b7706899f43dd782f68c52b8fc96d6
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-06 22:44:26 +0000

    Thu, Jan 6, 2022, 10:44 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -9,6 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for (int row=0; row<width-1; row++) {
         for (int column=0; column<height-1; column++) {
             RGBTRIPLE pixel = image[row][column];
+
             //take avg of each pixel rgbtRed, rgbtGreen, and rgbtBlue values and round them
             int pixel_color_avg = round(pixel.rgbtRed + pixel.rgbtGreen + pixel.rgbtBlue);
             pixel_color_avg /= 3;
