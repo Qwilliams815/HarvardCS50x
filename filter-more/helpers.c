90d650fee1069b67555b07ea89f82bfe99e2c6e0
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-07 01:18:58 +0000

    Fri, Jan 7, 2022, 1:18 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -9,17 +9,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     printf("test helloooooooo\n");
     for (int row=0; row<height; row++) {
         for (int column=0; column<width; column++) {
-            RGBTRIPLE pixel = image[row][column];
+            //RGBTRIPLE pixel = image[row][column];
 
             //take avg of each pixel rgbtRed, rgbtGreen, and rgbtBlue values and round them
-            int pixel_color_avg = round(pixel.rgbtRed + pixel.rgbtGreen + pixel.rgbtBlue);
+            int pixel_color_avg = round(image[row][column].rgbtRed + image[row][column].rgbtGreen + image[row][column].rgbtBlue);
             pixel_color_avg /= 3;
             //printf("%i ", pixel_color_avg);
 
             // Asign new values
-            pixel.rgbtRed = pixel_color_avg;
-            pixel.rgbtGreen = pixel_color_avg;
-            pixel.rgbtBlue = pixel_color_avg;
+            image[row][column].rgbtRed = pixel_color_avg;
+            image[row][column].rgbtGreen = pixel_color_avg;
+            image[row][column].rgbtBlue = pixel_color_avg;
         }
     }
     //set them all to that new rounded avg
