4ed551e79a70348e5dea6a61ca06a91aafbf5ec9
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-08 03:02:08 +0000

    Sat, Jan 8, 2022, 3:02 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -29,29 +29,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    //printf("Test\n");
-    //printf("%i\n", image[0][3].rgbtRed);
-    //RGBTRIPLE test = image[0][0];
-    //printf("%i\n", test.rgbtRed);
-    //int opposite = width-1;
+
     image[0][0].rgbtBlue = 255;
     image[0][0].rgbtGreen = 0;
     image[0][0].rgbtRed = 0;
 
-    //image[0][opposite].rgbtRed = 255;
-    //image[0][opposite].rgbtBlue = 0;
-    //image[0][opposite].rgbtGreen = 0;
-
-    //for (int row=0; row<height; row++) {
-    //    image[row][0].rgbtRed = 255;
-    //}
-
-
-    //RGBTRIPLE temp = image[0][opposite];
-    //image[0][opposite] = image[0][0];
-    //image[0][0] = temp;
-
-    //image[height-1][opposite].rgbtBlue = 255;
     printf("height: %i width: %i\n", height, width);
     //height 400, width: 600
 
