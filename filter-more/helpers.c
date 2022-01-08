dcd560508801773552607f06917de4b9eea75049
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-08 23:58:57 +0000

    Sat, Jan 8, 2022, 11:58 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -69,7 +69,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             for (int local_row = row-1; local_row<=row+1; local_row++) {
                 for (int local_column = column-1; local_column<=column+1; local_column++) {
                     if (image[local_row]) {
-                        printf("row does not exist");
+                        printf("row does exist\n");
+                        return 1;
 
                     }
                 //avg_red += image[local_row][local_column].rgbtRed;
