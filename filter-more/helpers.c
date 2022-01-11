261b9b71328484f81b5217b2a06f1395d5b183bd
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-11 09:26:33 +0000

    Tue, Jan 11, 2022, 9:26 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -112,7 +112,19 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     // Check if top Right
                     } else if (local_row == -1 && local_column == width) {
                         printf("Top Right \n");
-                        //top right handler
+                        for (int i = 0; i<=1; i++) {
+                            for (int j=0; j<= 1; j++) {
+                                avg_red += image[i][j].rgbtRed;
+                                avg_green += image[i][j].rgbtGreen;
+                                avg_blue += image[i][j].rgbtBlue;
+                                num_of_pixels++;
+                            }
+                        }
+                        printf("%i\n", num_of_pixels);
+                        image[row][column].rgbtRed = avg_red/num_of_pixels;
+                        image[row][column].rgbtGreen = avg_green/num_of_pixels;
+                        image[row][column].rgbtBlue = avg_blue/num_of_pixels;
+                        
                     // Check if bottom Left
                     } else if (local_row == height && local_column == -1) {
                         printf("Bottom Left \n");
