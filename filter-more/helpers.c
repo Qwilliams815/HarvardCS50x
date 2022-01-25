5619c6643af350cefaac18599f0e64f0414b8592
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-25 22:10:42 +0000

    Tue, Jan 25, 2022, 10:10 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -65,7 +65,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     for (int row=0; row<height; row++) {
         for (int column=0; column<width; column++) {
             image_copy[row][column] = image[row][column];
-            i++;
+            //i++;
         }
     }
 
