bfe0f03e4eb5ec81fc6c5615643036b3487d7780
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-25 04:56:41 +0000

    Tue, Jan 25, 2022, 4:56 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -68,6 +68,28 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             i++;
         }
     }
+
+    for (int row=0; row<height; row++) {
+        for (int column=0; column<width; column++) {
+            //image_copy[row][column] = image[row][column];
+            int avg_blue = 0;
+            int avg_green = 0;
+            int avg_red = 0;
+            int num_of_pixels = 0;
+
+            for (int local_row=-1; local_row<2; local_row++) {
+                for (int local_col=-1; local_col<2; local_col++) {
+                    if (row + local_row < 0 || row + local_row >= height) {
+                        continue;
+                    }
+                    if (column + local_col < 0 || column + local_col >= width)
+
+                }
+            }
+        }
+    }
+
+
     printf("pixels: %i\n", i);
 }
 
