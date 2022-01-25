90b95bb6acb20a4b51526ac365ee40544660fdfc
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-25 21:06:02 +0000

    Tue, Jan 25, 2022, 9:06 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -69,7 +69,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    // Loop through the pixels again 
+    // Loop through the pixels again, this time establishing avg color variables and accessing local pixels
     for (int row=0; row<height; row++) {
         for (int column=0; column<width; column++) {
             //image_copy[row][column] = image[row][column];
