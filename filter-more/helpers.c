125688f24fae3cffff53def5ae74ece01a27098a
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-26 02:20:16 +0000

    Wed, Jan 26, 2022, 2:20 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -111,5 +111,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Create copy of original image
+    RGBTRIPLE image_copy[height][width];
+    for (int row=0; row<height; row++) {
+        for (int column=0; column<width; column++) {
+            image_copy[row][column] = image[row][column];
+            //i++;
+        }
+    }
+
+
 }
