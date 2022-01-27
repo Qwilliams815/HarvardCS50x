2694a17699a44e62c40cda15a6fb89b269fb3cd9
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-27 09:48:22 +0000

    Thu, Jan 27, 2022, 9:48 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -49,10 +49,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     //int i = 0;
     // Create copy of original image
-    RGBTRIPLE image_copy[height][width];
+    //RGBTRIPLE image_copy[height][width];
     for (int row=0; row<height; row++) {
         for (int column=0; column<width; column++) {
-            image_copy[row][column] = image[row][column];
+            RGBTRIPLE image_copy[row][column] = image[row][column];
             //i++;
         }
     }
