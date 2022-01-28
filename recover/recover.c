4c1dc0a7aabd77d3697f0f6680c2ca40bd32f441
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-28 04:13:04 +0000

    Fri, Jan 28, 2022, 4:13 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,11 +14,12 @@ int main(int argc, char *argv[])
     // generated files must be 000.jpg, 001.jpg, ect.
 
     FILE *f = fopen(argv[1], "r");
+    FILE input = fread(data[], 512, all, argv[1]);
 
     // for each 512b block in argv[1]
     // if block starts with 0xff 0xd8 0xff, open a write file as current_file 000.
     // else write block to the current file
     // write every subsequent block to that file
-    
+
 
 }
