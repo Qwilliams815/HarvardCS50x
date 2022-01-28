f8eb6cffeae850274d72c403d6c78439ed7c78b5
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-28 04:04:28 +0000

    Fri, Jan 28, 2022, 4:04 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,5 +14,8 @@ int main(int argc, char *argv[])
     // generated files must be 000.jpg, 001.jpg, ect.
 
     FILE *f = fopen(argv[1], "r");
-    
+
+    // read file 512b block by 512b block
+    // if block starts with 0xff     0xd8     0xff, open a write file as 000.
+
 }
