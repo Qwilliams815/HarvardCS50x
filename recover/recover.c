f9822a10459040461bf16166063a0a6137068259
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-28 23:20:13 +0000

    Fri, Jan 28, 2022, 11:20 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     // if file cant be read, return 1
     FILE *finput = fopen(argv[1], "r");
     if (input == NULL)
-    
+
     {
         printf("Could not open file.\n");
         return 1;
@@ -24,6 +24,12 @@ int main(int argc, char *argv[])
 
     int buffer[512];
     fread(buffer, 1, 512, finput);
+
+    while (buffer[0] != EOF) {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
+         //open a write file as current_file 000
+     }
+    }
 
     // for each 512b block in argv[1]
     // if block starts with 0xff 0xd8 0xff,
