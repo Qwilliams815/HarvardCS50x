bdc139564d1eba5aee6364fac0cae7a5449f46b9
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-01-29 00:07:38 +0000

    Sat, Jan 29, 2022, 12:07 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -27,10 +27,10 @@ int main(int argc, char *argv[])
     int buffer[512];
     int count = 0;
     while (fread(buffer, 1, 512, finput) != 0) {
-        count++;
-        //if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
+            count++;
          //open a write file as current_file 000
-     //}
+        }
     }
     printf("%i\n", count);
 
