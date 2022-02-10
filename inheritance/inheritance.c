01ca803e29dd629abd5d8b71e1fdf87ad60442fa
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-02-10 20:23:34 +0000

    Thu, Feb 10, 2022, 8:23 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -90,11 +90,8 @@ void free_family(person *p)
     }
 
     // TODO: Free parents recursively
-    for (int i=0; i<GENERATIONS; i++)
-    {
         free_family(p->parents[0]);
         free_family(p->parents[1]);
-    }
 
     // TODO: Free child
     //if (p-parent)
