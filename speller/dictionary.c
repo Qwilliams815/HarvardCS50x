1d522dcfed1c89a5ece7dc65c2783b84a9f645a0
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-03-02 18:49:37 +0000

    Wed, Mar 2, 2022, 6:49 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -37,7 +37,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
+    // TODO: Take dictionary, load it into a hash table
     //If load succesful, return True, else return false
     bool loaded = true;
     FILE *dict = fopen(dictionary, "r");
