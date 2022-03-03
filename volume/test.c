e93759df16f7d6d86f3c4d302377c6c54ed7ad4f
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-03-03 20:39:13 +0000

    Thu, Mar 3, 2022, 8:39 PM +00:00

--- volume/test.c
+++ volume/test.c
@@ -10,10 +10,15 @@
 
 int main(void)
 {
+    char *word = "leHol";
+    int ascii_value = 0;
+    for (int i=0; i<strlen(word); i++) {
+        ascii_value += word[i];
+    }
     char letter1 = 'A';
     char letter2 = 'B';
     int sum = letter1 + letter2;
     printf("%i\n", letter1);
     printf("%i\n", letter2);
-    printf("%i\n", sum);
+    printf("%i\n", ascii_value);
 }
