fddf339d4275270eba71906f051b3bf08f0ea0f0
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-03-04 22:40:20 +0000

    Fri, Mar 4, 2022, 10:40 PM +00:00

---
+++ speller/file_manipulation.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    printf("test\n");
+    FILE *dict = fopen("dictionaries/large", "r");
+
+    // READING LINE BY LINE WITH SPECIFIED BUFFER SIZE
+    
+    // char word[45];
+    // while (fgets(word, 45, dict)) {
+    //     printf("%s", word);
+    // }
+
+    // char ch[1];
+    // while (fgetc(ch, 1, dict)) {
+    //     char word[];
+    // }
+
+    // READING CHARACTER BY CHARACTER (SLOWER)
+    char ch;
+    while ((ch = fgetc(dict)) != EOF)
+    {
+        printf("%c", ch);
+    };
+
+    fclose(dict);
+    return 0;
+}
