bec435da34c80cda640abd554ef3b5825d66c9a9
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-03-04 22:26:58 +0000

    Fri, Mar 4, 2022, 10:26 PM +00:00

--- speller/test.c
+++ speller/test.c
@@ -6,10 +6,20 @@ int main(void)
 {
     printf("test\n");
     FILE *dict = fopen("dictionaries/large", "r");
-    char word[45];
-    while (fgets(word, 45, dict)) {
-        printf("%s", word);
-    }
+    //char word[45];
+    //while (fgets(word, 45, dict)) {
+    //    printf("%s", word);
+    //}
+
+    //char ch[1];
+    //while (fgetc(ch, 1, dict)) {
+    //    char word[];
+    //}
+    string word = "Hello";
+    word += " world!";
+
+    printf("%s", word);
+
     fclose(dict);
     return 0;
 }
