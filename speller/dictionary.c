15a8ff606b4aff70ec23d7e58c28c5f0868b37c7
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-03-31 18:52:50 +0000

    Thu, Mar 31, 2022, 6:52 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -30,10 +30,10 @@ bool check(const char *word)
     // TODO
 
     // Create lowercase version of word
-    char lower_word[LENGTH + 1];
+    char lower_word[LENGTH];
     for (int i = 0; i < strlen(word); i++)
     {
-        lower_word[i] = tolower(*word[i]);
+        lower_word[i] = tolower(word[i]);
     }
 
     // Check table index of hash(lower_word)
