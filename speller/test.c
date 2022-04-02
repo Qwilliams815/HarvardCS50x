2e7deee38db16e7cde188a5ead13f76460363e32
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-02 06:46:09 +0000

    Sat, Apr 2, 2022, 6:46 AM +00:00

--- speller/test.c
+++ speller/test.c
@@ -4,133 +4,133 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-//#include <math.h>
+// //#include <math.h>
 
-// Declare max length of any given dictionary word
-#define LENGTH 45
+// // Declare max length of any given dictionary word
+// #define LENGTH 45
 
-// 1. Create Node object
-typedef struct node
-{
-    char word[LENGTH + 1];
-    struct node *next;
-} node;
+// // 1. Create Node object
+// typedef struct node
+// {
+//     char word[LENGTH + 1];
+//     struct node *next;
+// } node;
 
-// Set Estimated Node list length based on amount of given Keys (dictionary words)
-const unsigned int N = 250;
+// // Set Estimated Node list length based on amount of given Keys (dictionary words)
+// const unsigned int N = 250;
 
-// 2. Create table for nodes
-node *table[N];
+// // 2. Create table for nodes
+// node *table[N];
 
-// 3. Hash Function for finding the index of a Word
-unsigned int hash(const char *word)
-{
-    int ascii = 0;
-    for (int i = 0; i < strlen(word); i++)
-    {
-        ascii += word[i];
-    }
+// // 3. Hash Function for finding the index of a Word
+// unsigned int hash(const char *word)
+// {
+//     int ascii = 0;
+//     for (int i = 0; i < strlen(word); i++)
+//     {
+//         ascii += word[i];
+//     }
 
-    ascii *= strlen(word);
-    ascii *= word[0];
-    // double new = pow(ascii, strlen(word));
-    // return printf("%s -- %i\n\n", word, ascii % 2500);
-    return ascii % N;
-}
+//     ascii *= strlen(word);
+//     ascii *= word[0];
+//     // double new = pow(ascii, strlen(word));
+//     // return printf("%s -- %i\n\n", word, ascii % 2500);
+//     return ascii % N;
+// }
 
-// Load Dictionary words into Hash Table
-bool load(const char *dictionary)
-{
-    // TODO: Take dictionary, load it into a hash table
-    // If load succesful, return True, else return false
-    bool loaded = true;
-    FILE *dict = fopen(dictionary, "r");
+// // Load Dictionary words into Hash Table
+// bool load(const char *dictionary)
+// {
+//     // TODO: Take dictionary, load it into a hash table
+//     // If load succesful, return True, else return false
+//     bool loaded = true;
+//     FILE *dict = fopen(dictionary, "r");
 
-    if (dict == NULL)
-    {
-        printf("Could not open Dictionary.\n");
-        loaded = false;
-    }
+//     if (dict == NULL)
+//     {
+//         printf("Could not open Dictionary.\n");
+//         loaded = false;
+//     }
 
-    // Scan words from dictionary file
-    char word[45];
-    while (fgets(word, 45, dict))
-    {
-        // Allocate Memory for node
-        node *n = malloc(sizeof(node));
+//     // Scan words from dictionary file
+//     char word[45];
+//     while (fgets(word, 45, dict))
+//     {
+//         // Allocate Memory for node
+//         node *n = malloc(sizeof(node));
 
-        // Fill node with word and next address
-        strcpy(n->word, word);
-        n->next = NULL;
+//         // Fill node with word and next address
+//         strcpy(n->word, word);
+//         n->next = NULL;
 
-        // Assign node to linked hashmap spot based on hash function
-        // Get hash index of word
-        int word_index = hash(word);
+//         // Assign node to linked hashmap spot based on hash function
+//         // Get hash index of word
+//         int word_index = hash(word);
 
-        // Populate new node properties
-        table[word_index] = n;
-    }
+//         // Populate new node properties
+//         table[word_index] = n;
+//     }
 
-    return loaded;
-}
+//     return loaded;
+// }
 
-// Main Execution
-int main(void)
+// // Main Execution
+// int main(void)
 
-{
-    // printf("test\n");
-    FILE *dict = fopen("dictionaries/large", "r");
+// {
+//     // printf("test\n");
+//     FILE *dict = fopen("dictionaries/large", "r");
 
-    // READING LINE BY LINE WITH SPECIFIED BUFFER SIZE
-    char word[45];
-    while (fgets(word, 45, dict))
-    {
-        // create new n node for each word
-        // printf("%s", word);
-        // printf("%i -- %s", hash(word), word);
+//     // READING LINE BY LINE WITH SPECIFIED BUFFER SIZE
+//     char word[45];
+//     while (fgets(word, 45, dict))
+//     {
+//         // create new n node for each word
+//         // printf("%s", word);
+//         // printf("%i -- %s", hash(word), word);
 
-        node *n = malloc(sizeof(node));
+//         node *n = malloc(sizeof(node));
 
-        // Place empty n node in at hash(word)'s index
-        table[hash(word)] = n;
-        strcpy(n->word, word);
+//         // Place empty n node in at hash(word)'s index
+//         table[hash(word)] = n;
+//         strcpy(n->word, word);
 
-        // hash(word);
+//         // hash(word);
 
-        // Populate n node properties
-        if (table[hash(word)] == NULL)
-        {
-            strcpy(n->word, word);
-            // printf("%s, %s\n", n->word, word);
-            //  table[hash(word)]->word = word;
-        }
-        else
-        {
-            continue;
-        }
+//         // Populate n node properties
+//         if (table[hash(word)] == NULL)
+//         {
+//             strcpy(n->word, word);
+//             // printf("%s, %s\n", n->word, word);
+//             //  table[hash(word)]->word = word;
+//         }
+//         else
+//         {
+//             continue;
+//         }
 
-        // if (strlen(word) == 12)
-        // {
-        //     break;
-        // }
-    }
+//         // if (strlen(word) == 12)
+//         // {
+//         //     break;
+//         // }
+//     }
 
-    for (int i = 0; i < N; i++)
-    {
-        if (table[i] == NULL)
-        {
-            //printf("%i -- %s\n", i, table[i]->word);
-        }
-        else
-        {
-            //printf("%i -- %s", i, table[i]->word);
-        }
-    }
+//     for (int i = 0; i < N; i++)
+//     {
+//         if (table[i] == NULL)
+//         {
+//             //printf("%i -- %s\n", i, table[i]->word);
+//         }
+//         else
+//         {
+//             //printf("%i -- %s", i, table[i]->word);
+//         }
+//     }
 
-    // Testing
-    // current item =
-    // while current item != Null
-    //     current item = current_item-next;
+//     // Testing
+//     // current item =
+//     // while current item != Null
+//     //     current item = current_item-next;
 
     const char *greeting = "HELLO";
 
@@ -153,7 +153,7 @@ int main(void)
 
     // printf("%i\n", toupper(greeting[0]) - 'A');
     free(lower_word);
-    fclose(dict);
+    //fclose(dict);
 
 
 
