634d786b54130a6b239b2f2a90d274b13427e49e
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-06 21:49:16 +0000

    Wed, Apr 6, 2022, 9:49 PM +00:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -5,33 +5,38 @@ int main(void)
 {
     // get user input (1-8) for number of rows
     int height;
-    do {
+    do
+    {
         height = get_int("Height: ");
     } while (1 > height || height > 8);
 
     int row_length = height;
     int row_index = 1;
 
-    //Establishing/iterating number of rows
-    while (height > 0) {
+    // Establishing/iterating number of rows
+    while (height > 0)
+    {
         int num_of_hashtags = row_index;
         int spaces = row_length - row_index;
 
-        //Printing Spaces in each row
-        for (int i=0; i < spaces; i++) {
+        // Printing Spaces in each row
+        for (int i = 0; i < spaces; i++)
+        {
             printf(" ");
         };
 
-        //Printing First Half of #'s
-        for (int i=0; i < num_of_hashtags; i++) {
+        // Printing First Half of #'s
+        for (int i = 0; i < num_of_hashtags; i++)
+        {
             printf("#");
         };
 
-        //Splitting the Pyramid
+        // Splitting the Pyramid
         printf("  ");
 
-        //Printing Second Half of #'s
-        for (int i=0; i < num_of_hashtags; i++) {
+        // Printing Second Half of #'s
+        for (int i = 0; i < num_of_hashtags; i++)
+        {
             printf("#");
         };
 
@@ -40,5 +45,4 @@ int main(void)
         row_index++;
         height--;
     }
-
 }
