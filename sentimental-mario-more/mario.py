15627b3d3c85cd4842412408538a667ecb75a122
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-06 22:36:51 +0000

    Wed, Apr 6, 2022, 10:36 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,7 +2,7 @@ from cs50 import get_int
 # Gather pyramid height from user input
 height = get_int("Height: ")
 
-if 1 > height > 8:
+while height < 1 or height > 8 or isalpha(height) == True:
     height = get_int("Height: ")
 
 
