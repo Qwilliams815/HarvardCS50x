434e7332db3a4998936a2994a475ce5294a6b7c1
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-07 21:09:52 +0000

    Thu, Apr 7, 2022, 9:09 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,7 @@
 from cs50 import get_string
 
 # TODO input some text, output the grade level for the text
+text = get_string("Text: ")
 
 # TODO Coleman-Liau formula 0.0588 * L - 0.296 * S - 15.8
 # L is avg letters per 100 words
