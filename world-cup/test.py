1838dc1c354676834146a3bb550ad5904dd9d0f4
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-07 17:07:35 +0000

    Thu, Apr 7, 2022, 5:07 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -1,8 +1,8 @@
 import re
 
 
-number = "371449635398431"
-number_patter = "[0-9]"
+number = "37144963dasdDSA53984asd31"
+number_pattern = "[0-9]"
 
 if (re.search(number_pattern, number)):
     print("valid")
@@ -10,23 +10,23 @@ else:
     print("invalid")
 
 
-amex = ["34", "37"]
-master_card = ["51", "52", "53", "54", "55"]
+# amex = ["34", "37"]
+# master_card = ["51", "52", "53", "54", "55"]
 
-# 371449635398431 AMEX
+# # 371449635398431 AMEX
 
-num_sum_1 = 0
-for num in number[-2::-2]:
-    num_sum_1 += int(num) * 2
-    print(num)
-print("sum1:", num_sum_1)
+# num_sum_1 = 0
+# for num in number[-2::-2]:
+#     num_sum_1 += int(num) * 2
+#     print(num)
+# print("sum1:", num_sum_1)
 
-num_sum_2 = 0
-for num in number[-1::-2]:
-    num_sum_2 += int(num)
-    print(num)
-print("sum2:", num_sum_2)
+# num_sum_2 = 0
+# for num in number[-1::-2]:
+#     num_sum_2 += int(num)
+#     print(num)
+# print("sum2:", num_sum_2)
 
-if (num_sum_1 + num_sum_2) % 10 != 0:
-    print("INVALID3")
-    quit()
+# if (num_sum_1 + num_sum_2) % 10 != 0:
+#     print("INVALID3")
+#     quit()
