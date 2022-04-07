0f357c9997bbb286003bc401ecb9d5ca20c5b822
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-07 03:38:14 +0000

    Thu, Apr 7, 2022, 3:38 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -14,7 +14,9 @@ num_sum_2 = 0
 for num in number[-1::-2]:
     num_sum_2 += num
 
-if (num_sum_1 + num_sum_2) % 10 == 0;
-    return True
+if (num_sum_1 + num_sum_2) % 10 != 0;
+    print("INVALID")
+
+if number.startswith()
 
 
