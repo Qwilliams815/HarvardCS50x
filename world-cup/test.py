f8c28196c2660a24f56121d1a8d8cfdb7f99f271
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-07 19:56:19 +0000

    Thu, Apr 7, 2022, 7:56 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -13,25 +13,3 @@ for num in master_card:
     if number.startswith(num) and len(number) == 16:
         print("MASTERCARD")
         quit()
-
-
-# amex = ["34", "37"]
-# master_card = ["51", "52", "53", "54", "55"]
-
-# # 371449635398431 AMEX
-
-# num_sum_1 = 0
-# for num in number[-2::-2]:
-#     num_sum_1 += int(num) * 2
-#     print(num)
-# print("sum1:", num_sum_1)
-
-# num_sum_2 = 0
-# for num in number[-1::-2]:
-#     num_sum_2 += int(num)
-#     print(num)
-# print("sum2:", num_sum_2)
-
-# if (num_sum_1 + num_sum_2) % 10 != 0:
-#     print("INVALID3")
-#     quit()
