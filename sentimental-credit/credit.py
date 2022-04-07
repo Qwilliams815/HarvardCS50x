fb708ffb634f448fd54e0fcae2aea538df9dd116
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-07 04:59:44 +0000

    Thu, Apr 7, 2022, 4:59 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -17,10 +17,12 @@ if len(number) < 13 or len(number) > 16:
 num_sum_1 = 0
 for num in number[-2::-2]:
     num_sum_1 += int(num) * 2
+print(num_sum_1)
 
 num_sum_2 = 0
 for num in number[-1::-2]:
     num_sum_2 += int(num)
+print(num_sum_2)
 
 if (num_sum_1 + num_sum_2) % 10 != 0:
     print("INVALID3")
