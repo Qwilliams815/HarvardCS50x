4d4b9a7943d1a4813edd73a53cb0e88680b90207
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-07 03:52:40 +0000

    Thu, Apr 7, 2022, 3:52 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,28 +1,29 @@
 from cs50 import get_int
 
-number = get_int("Number: ")
+number = input("Number: ")
 
 num_sum_1 = 0
 for num in number[-2::-2]:
-    num_sum_1 += num * 2
+    num_sum_1 += int(num) * 2
 
 num_sum_2 = 0
 for num in number[-1::-2]:
-    num_sum_2 += num
+    num_sum_2 += int(num)
 
 if (num_sum_1 + num_sum_2) % 10 != 0:
     print("INVALID")
+    quit()
 
 amex = ["34", "37"]
 master_card = ["51", "52", "53", "54", "55"]
 
 for num in amex:
-    if str(number).startswith(num):
+    if number.startswith(num):
         print("AMEX")
 for num in master_card:
-    if str(number).startswith(num):
+    if number.startswith(num):
         print("MASTERCARD")
-if str(number)[0] == "4":
+if number[0] == "4":
     print("VISA")
 
 
