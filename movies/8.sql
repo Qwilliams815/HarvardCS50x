13e5d12eb310b23fafc5cbb8bb92d71a17444c61
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-18 22:13:30 +0000

    Mon, Apr 18, 2022, 10:13 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,2 @@
 -- List the names of all people who starred in Toy Story.
-SELECT name FROM people WHERE id IN (SELECT id FROM movies WHERE title = "Toy Story");
-
-Toy story has an id
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE title = "Toy Story"));
