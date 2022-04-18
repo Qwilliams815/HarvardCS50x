6e9af6f52a2fadcd970a991f4b2bf0e53cb51d79
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-18 19:12:49 +0000

    Mon, Apr 18, 2022, 7:12 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,2 +1,2 @@
 -- List all movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
-SELECT title, rating FROM movies, ratings LIMIT 10;
+SELECT movie.title, ratings.rating FROM movies, ratings LIMIT 10;
