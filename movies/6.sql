9ade9d6431ae1b9f23852e1e62c95d48851dda5d
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-18 18:18:47 +0000

    Mon, Apr 18, 2022, 6:18 PM +00:00

--- movies/6.sql
+++ movies/6.sql
@@ -2,3 +2,6 @@
 -- SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE year = 2012);
 SELECT AVG(rating) FROM ratings WHERE movie_id = 2012;
 SELECT id FROM movies WHERE year = 2012;
+
+SELECT id FROM movies LIMIT 10;
+SELECT movie_id FROM ratings LIMIT 10;
