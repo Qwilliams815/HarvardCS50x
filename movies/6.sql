7542f5a3bd246652cb7fe7fa4d056213f453ecc5
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-18 18:16:18 +0000

    Mon, Apr 18, 2022, 6:16 PM +00:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,3 +1,4 @@
 -- Determine the average rating of all movies released in 2012
 -- SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE year = 2012);
+SELECT AVG(rating) FROM ratings WHERE movie_id = 2012;
 SELECT id FROM movies WHERE year = 2012;
