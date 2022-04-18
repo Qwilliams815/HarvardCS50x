479cdc7bfa051e74cfe1f38a3691e314fd064eba
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-18 23:10:24 +0000

    Mon, Apr 18, 2022, 11:10 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,2 +1,4 @@
 -- List the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
-SELECT name FROM movies WHERE 
+SELECT name FROM movies WHERE id IN (SELECT )
+
+Movies - > with chandwick -> rated highly -> orderby blank desc -> limit 5
