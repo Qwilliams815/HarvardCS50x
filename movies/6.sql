1789ecd54fd665e5c394aeb1ab1914941e3343dd
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-18 18:55:56 +0000

    Mon, Apr 18, 2022, 6:55 PM +00:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,11 +1,2 @@
 -- Determine the average rating of all movies released in 2012
--- SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE year = 2012);
-SELECT rating FROM ratings WHERE movie_id = 2012;
-SELECT id FROM movies WHERE year = 2012;
-
-SELECT id FROM movies LIMIT 10;
-SELECT movie_id FROM ratings LIMIT 10;
-
-SELECT AVG(rating) FROM ratings JOIN ON movies.id = ratings.movie_id;
-
-sqlite> SELECT rating FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE title LIKE "%Harry Potter%");
+SELECT AVG(rating) FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2012);
