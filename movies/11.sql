f25a8086ea1f216228a866193b44e5c0aa2b073a
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-19 17:12:08 +0000

    Tue, Apr 19, 2022, 5:12 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,10 +3,6 @@ SELECT name FROM movies WHERE id IN
 (SELECT movie_id FROM ratings WHERE movie_id IN
 (SELECT id FROM people WHERE name = "Chadwick Boseman"))
 
-Movies -> rated highly -> with chadwick -> orderby rating desc -> limit 5
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC) LIMIT 5;
 
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings ORDER BY rating) LIMIT 10;
-
-Movies -> ratings -> people -> stars -> chadwick
-
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman"));
+SELECT movies.title, ratings.rating FROM movies INNER JOIN ratings.movie_id ON movies.id WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC);
