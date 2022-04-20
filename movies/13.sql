01c447447ca1637ad9efb9c6636c09add32083d9
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-20 20:16:52 +0000

    Wed, Apr 20, 2022, 8:16 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,9 @@
 -- List the names of all people who starred in a movie in which Kevin Bacon also starred.
+select all people in movie where movie id IN (find all movies that kevin bacon was in)
 
 SELECT name FROM people WHERE id IN
-(SELECT person_id FROM stars)
+(SELECT person_id FROM stars WHERE )
+
+
+SELECT count(movie_id) from STARS where person_id = (SELECT id FROM people WHERE name = "Kevin Bacon");
+
