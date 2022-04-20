5bff1c63970b04572141bf8b52bfceeb2fdbd74a
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-20 04:31:16 +0000

    Wed, Apr 20, 2022, 4:31 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -5,4 +5,5 @@ SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id
 select movie_id where johnny and helena IN
 
 -- Try this
-Select movie id where id = (select id of all jonny movies) = (select id's of all helans movies);
+Select movie id where id = (select id of all jonny movies) = (select id's of all helans movies)';
+-- Yikes
