71c068de42b9270de89300e81258cdc68c5c26df
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-20 18:56:09 +0000

    Wed, Apr 20, 2022, 6:56 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -49,4 +49,7 @@ SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name 
 
 SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter")) GROUP BY movie_id HAVING COUNT(*) > 1);
 
+
 SELECT title, id FROM movies WHERE title LIKE "%Lone Ranger%";
+
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id = 1210819);
