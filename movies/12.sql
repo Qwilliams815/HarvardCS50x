a7e5a795f867edddf507d1360ee9b043f7a3b92d
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-20 17:21:03 +0000

    Wed, Apr 20, 2022, 5:21 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -32,5 +32,7 @@ Select id from johnny movies where id in (select id of all of helnas movies)
 
 SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter")));
 
-SELECT movie_id FROM stars WHERE mo(SELECT movie_id FROM stars WHERE person_id IN (SELECT ID FROM people WHERE name = "Johnny Depp")) = (SELECT movie_id FROM stars WHERE person_id IN (SELECT ID FROM people WHERE name = "Helena Bonham Carter"));
+SELECT movie_id FROM stars WHERE movie_id IN
+(SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp") IN
+(SELECT movie_id FROM stars WHERE person_id IN (SELECT ID FROM people WHERE name = "Helena Bonham Carter"));
 
