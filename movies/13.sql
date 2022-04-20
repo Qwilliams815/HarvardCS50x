1a5e3c8fd7c1399475307f3c4b97d88e5fe64d6d
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-20 20:26:35 +0000

    Wed, Apr 20, 2022, 8:26 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -7,7 +7,7 @@ SELECT name FROM people WHERE id IN
 
 SELECT name FROM people WHERE id IN
  (SELECT person_id FROM stars WHERE movie_id IN
- (SELECT movie_id from STARS where person_id = (SELECT id FROM people WHERE name = "Kevin Bacon")));
+ (SELECT movie_id from STARS where person_id = (SELECT id FROM people WHERE name = "Kevin Bacon"))) AND name != "Kevin Bacon";
 
 SELECT movie_id from STARS where person_id = (SELECT id FROM people WHERE name = "Kevin Bacon");
 
