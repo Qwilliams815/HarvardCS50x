8d6cd5fb2ff3d7d63c7195c74b8ffd2dabdb9176
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-20 07:08:18 +0000

    Wed, Apr 20, 2022, 7:08 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -4,9 +4,10 @@ SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id
 
 SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp")) AND
  (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter"));
- 
+
 select movie_id where johnny and helena IN
 
 -- Try this
 Select movie id where id = (select id of all jonny movies) = (select id's of all helans movies)';
+SELECT title FROM movies WHERE id IN ((SELECT ))
 -- Yikes
