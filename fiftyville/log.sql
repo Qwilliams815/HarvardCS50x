9cdbd9efec55027a1ce453a57776ba28d58f9d6c
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-22 17:19:57 +0000

    Fri, Apr 22, 2022, 5:19 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -47,15 +47,13 @@ SELECT name
 FROM people
 WHERE license_plate IN
     (
-        "5P2BI95",
-        "94KL13X",
-        "6P58WS2",
-        "4328GD8",
-        "G412CB7",
-        "L93JTIZ",
-        "322W7JE",
-        "0NTHK55"
-    )
+        SELECT license_plate
+        FROM bakery_security_logs
+        WHERE hour = 10
+        AND minute > 15
+        AND minute < 26
+        AND year = 2021 AND month = 7 AND day = 28
+    );
 
 -- Eugene recommends checking ATM withdrawal logs on Leggett Street
 SELECT
