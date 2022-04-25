6990d0269f0bf69338d79ef7bf82e2ac9bf997a9
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-25 21:16:20 +0000

    Mon, Apr 25, 2022, 9:16 PM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -1,6 +1,7 @@
 function correct_answer() {
-    document.queryselector('.correct')
-}
+    const correct_text = document.queryselector('.correct');
+    correct_text.classList.toggle('correct_popup_color');
+};
 
 function incorrect_answer() {
 
