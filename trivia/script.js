6bdaa326dcbf8a4f278d9817542d2c753da3ac26
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-25 21:21:15 +0000

    Mon, Apr 25, 2022, 9:21 PM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -1,9 +1,11 @@
 function correct_answer() {
     const correct_text = document.queryselector('.correct');
-    correct_text.classList.toggle('correct_popup_color');
+    correct_text.style.backgroundColor = 'green';
 };
 
 function incorrect_answer() {
+    const incorrect_correct_text = document.queryselector('.incorrect');
+    correct_text.style.backgroundColor = 'red';
 
 }
 
