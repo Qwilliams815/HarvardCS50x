a03c80920e1f37080ff51b4c8cb3fdd6f0f20ff5
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-26 15:50:32 +0000

    Tue, Apr 26, 2022, 3:50 PM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -10,7 +10,6 @@
 // }
 const correct_text = document.querySelector('.correct_popup');
 const wrong_text = document.querySelector('.incorrect_popup');
-let text_status = false;
 
 const wrong_answer_1 = document.querySelector('.wrong_1');
 const wrong_answer_2 = document.querySelector('.wrong_2');
@@ -18,14 +17,20 @@ const correct_answer = document.querySelector('.correct');
 
 wrong_answer_1.addEventListener('click', function() {
     wrong_answer_1.style.backgroundColor = 'red';
+    wrong_text.classList.add('show_popup');
+    correct_text.classList.remove('show_popup');
 
 })
 
 wrong_answer_2.addEventListener('click', function() {
     wrong_answer_2.style.backgroundColor = 'red';
+    wrong_text.classList.add('show_popup');
+    correct_text.classList.remove('show_popup');
 })
 
 correct_answer.addEventListener('click', function() {
     correct_answer.style.backgroundColor = 'green';
+    correct_text.classList.add('show_popup');
+    wrong_text.classList.remove('show_popup');
 })
 
