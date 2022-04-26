268572a2b13e07d34ce98e05f1c4a96bb01055b0
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-04-26 15:42:11 +0000

    Tue, Apr 26, 2022, 3:42 PM +00:00

--- trivia/script.js
+++ trivia/script.js
@@ -1,11 +1,27 @@
-function correct_answer() {
-    const correct_text = document.querySelector('.correct');
-    correct_text.style.backgroundColor = 'green';
-    console.log("hello world");
-};
+// function correct_answer() {
+//     const correct_text = document.querySelector('.correct');
+//     correct_text.style.backgroundColor = 'green';
+//     console.log("hello world");
+// };
 
-function wrong_answer() {
-    const wrong_text = document.querySelector('.wrong');
-    wrong_text.style.backgroundColor = 'red';
-}
+// function wrong_answer() {
+//     const wrong_text = document.querySelector('.wrong');
+//     wrong_text.style.backgroundColor = 'red';
+// }
+
+const wrong_answer_1 = document.querySelector('.wrong_1');
+const wrong_answer_2 = document.querySelector('.wrong_2');
+const correct_answer = document.querySelector('.correct');
+
+wrong_answer_1.addEventListener('click', function() {
+    wrong_answer_1.style.backgroundColor = 'red';
+})
+
+wrong_answer_2.addEventListener('click', function() {
+    wrong_answer_1.style.backgroundColor = 'red';
+})
+
+correct_answer.addEventListener('click', function() {
+    correct_answer.style.backgroundColor = 'green';
+})
 
