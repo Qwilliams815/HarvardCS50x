01a889d0cce6caa4ce7dc95f4160f153cd4b7fae
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-06 02:32:12 +0000

    Mon, Jun 6, 2022, 2:32 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -139,7 +139,7 @@ def register():
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        # Add user to user
+        # Add user to user database
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
