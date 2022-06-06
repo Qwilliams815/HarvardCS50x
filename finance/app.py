3db20f4810ad4ca0d6dd1ffb62204d878f934e44
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-06 02:17:11 +0000

    Mon, Jun 6, 2022, 2:17 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -119,7 +119,32 @@ def quote():
 def register():
     """Register user"""
     if request.method == "POST":
-        pass
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Ensure password confirmation was submitted and a match
+        elif not request.form.get("confirm-password") or request.form.get("confirm-password") != request.form.get("password"):
+            return apology("Password confirmation must match Password")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?",
+                          request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
     else:
         return render_template("register.html")
 
