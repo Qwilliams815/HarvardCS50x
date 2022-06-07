df85bbaa999ddeb18caf962df509017af29b6fb2
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-07 15:40:34 +0000

    Tue, Jun 7, 2022, 3:40 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -132,6 +132,7 @@ def register():
             return apology("Password confirmation must match Password")
 
         else:
+            id = len(db)
 
             # Add user to user database
             db.execute(f"INSERT INTO users (id, username, hash) VALUES ({id}, {request.form.get("username")}, {generate_password_hash(request.form.get("password"))})")
