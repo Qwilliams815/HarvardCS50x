20e072342b9b64f024bfa4fc56e7858993f7a997
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-11 19:38:44 +0000

    Sat, Jun 11, 2022, 7:38 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -53,7 +53,7 @@ def index():
     except:
         db.execute("CREATE TABLE portfolio (symbol TEXT NOT NULL, name TEXT NOT NULL, shares INTEGER, price FLOAT, total FLOAT, cash NUMERIC NOT NULL DEFAULT 10000.00)")
 
-    return render_template("index.html", portfolio=db.execute("SELECT * FROM portfolio"), cash=db.execute("SELECT cash FROM users))
+    return render_template("index.html", portfolio=db.execute("SELECT * FROM portfolio"), cash=db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"]))
 
 
 @app.route("/buy", methods=["GET", "POST"])
