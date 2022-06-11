9332cda9e6188019c68876938014f7c155b534ae
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-11 22:42:13 +0000

    Sat, Jun 11, 2022, 10:42 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -51,7 +51,7 @@ def index():
     try:
         db.execute("SELECT * FROM portfolio")
     except:
-        db.execute("CREATE TABLE portfolio (symbol TEXT NOT NULL, name TEXT NOT NULL, shares INTEGER, price FLOAT, total FLOAT, cash NUMERIC NOT NULL DEFAULT 10000.00)")
+        db.execute("CREATE TABLE portfolio (symbol TEXT NOT NULL, name TEXT NOT NULL, shares INTEGER, price FLOAT, total FLOAT)")
 
     return render_template("index.html", portfolio=db.execute("SELECT * FROM portfolio"), cash=db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"]))
     # cash from users, everything else from portfolio.
