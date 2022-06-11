c25f0080423765a7330f85d9e4ad964500391335
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-11 16:16:38 +0000

    Sat, Jun 11, 2022, 4:16 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -77,8 +77,12 @@ def buy():
 
         else:
 
-            total_cost = symbol['price'] * shares
-            print(total_cost)
+            total_cost = symbol['price'] * int(shares)
+            portfolio_cash = db.execute("SELECT cash FROM portfolio")
+
+            if portfolio_cash - total_cost < 0:
+                return apology("Insufficient Funds")
+            return render_template("buy.html")
             # if db.execute("SELECT cash FROM users WHERE ) users cash amount - (amount of shares X cost) is negative, if so, throw error.
             # else, subtract purchased amount from cash amount and update users table
             # new table needs symbol, stock name, # of shares, price per share, total spent so far. display cash and purchaseing power at top.
