2ae00a91c0303a48476263ea46dac1143a3e91f8
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-11 18:13:18 +0000

    Sat, Jun 11, 2022, 6:13 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -80,7 +80,7 @@ def buy():
             total_cost = symbol['price'] * shares
             portfolio_cash = db.execute("SELECT cash FROM portfolio")
             print(portfolio_cash)
-            #interesting but also broken
+            #interesting but
 
             if portfolio_cash - total_cost < 0:
                 return apology("Insufficient Funds")
