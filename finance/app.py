2a7d230ae5ac33b7477246e0e313c7532d510bdb
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-13 19:17:54 +0000

    Mon, Jun 13, 2022, 7:17 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -49,7 +49,6 @@ def index():
     # db.execute("DROP TABLE portfolio")
     # db.execute("UPDATE users SET cash = ? WHERE id = ?", 10000.00, session["user_id"])
 
-    # print(symbol['symbol'] in db.execute("SELECT symbol FROM portfolio WHERE user_portfolio_id = ?", session["user_id"]))
 
     try:
         db.execute("SELECT * FROM portfolio")
@@ -68,6 +67,8 @@ def buy():
     """Buy shares of stock"""
 
     if request.method == "POST":
+
+        print(symbol['symbol'] in db.execute("SELECT symbol FROM portfolio WHERE user_portfolio_id = ?", session["user_id"]))
 
         # Grab symbol and shares from form
         symbol = lookup(request.form.get("symbol"))
