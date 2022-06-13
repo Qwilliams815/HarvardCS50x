56f092994f93da12bef6116589e6fe87ea185a30
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-13 17:33:46 +0000

    Mon, Jun 13, 2022, 5:33 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -90,7 +90,9 @@ def buy():
             else:
 
                 if symbol['symbol'] in db.execute("SELECT symbol FROM portfolio WHERE user_portfolio_id = ?", session["user_id"]):
-                    db.execute("UPDATE portfolio ())
+                    current_shares = db.execute("SELECT shares FROM portfolio WHERE symbol = )
+                    current_total =
+                    db.execute("UPDATE portfolio (shares, total) WHERE symbol = ?", )
                     #already symbol already in portfolio, just add the shares and tota
                 else:
                     db.execute("INSERT INTO portfolio (user_portfolio_id, symbol, name, shares, price, total) VALUES (?, ?, ?, ?, ?, ?)", session["user_id"], symbol['symbol'], symbol['name'], shares, symbol['price'], total_cost)
