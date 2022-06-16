a69b310c89d44a176c84a2c8643a2175d7148092
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-16 07:36:31 +0000

    Thu, Jun 16, 2022, 7:36 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -243,9 +243,9 @@ def sell():
             # Update shares amount and total price, shares = current shares - shares; total = symbol['total'] - symbol['price'] * shares
             price = db.execute("SELECT price FROM portfolio WHERE symbol = ? AND user_portfolio_id = ?", chosen_symbol, session['user_id'])
             total = db.execute("SELECT total FROM portfolio WHERE symbol = ? AND user_portfolio_id = ?", chosen_symbol, session['user_id'])
-            db.execute("UPDATE portfolios SET (shares, total) = ?, ? WHERE user_portfolio_id", current_shares-shares, )
+            db.execute("UPDATE portfolios SET (shares, total) = ?, ? WHERE user_portfolio_id", current_shares-shares, total-price*shares)
 
-        # update amount of shares/total from the other thing
+            render 
 
     else:
         print(symbols)
