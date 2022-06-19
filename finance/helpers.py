387bafafd24c96dbc88d499dd78aec94e68c3197
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-19 04:37:30 +0000

    Sun, Jun 19, 2022, 4:37 AM +00:00

--- finance/helpers.py
+++ finance/helpers.py
@@ -66,7 +66,7 @@ def usd(value):
 
 
 def validate_symbol(symbol):
-    # or symbol['symbol'].isalpha() == False
+    # or symbol['symbol'].isalpha() == False why is this not returning?
     if not symbol or symbol == None:
         print("WE GOT HERE ~~~~~~~~~")
         return apology("Invalid Symbol", 403)
