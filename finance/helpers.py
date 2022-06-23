355d8a4baed5aab3431df1055f845b1c74841225
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-23 19:03:33 +0000

    Thu, Jun 23, 2022, 7:03 PM +00:00

--- finance/helpers.py
+++ finance/helpers.py
@@ -74,4 +74,4 @@ def validate_symbol(symbol):
 def validate_shares(shares):
     if not shares  or type(shares) != int or int(shares) < 1:
         return apology("Invalid Shares Amount", 400)
-        # what if shares is float? huh
+        # what if shares is float?
