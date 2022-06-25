41db7edeaf87db393952033c3e589a7843d1e018
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2022-06-25 05:15:04 +0000

    Sat, Jun 25, 2022, 5:15 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -6,7 +6,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd, validate_symbol, validate_shares
+from helpers import apology, login_required, lookup, usd, validate_symbol, validate_shares, return_test
 
 # Configure application
 app = Flask(__name__)
@@ -134,6 +134,7 @@ def buy():
 
 
     else:
+        return return_test()
         return render_template("buy.html")
 
 
